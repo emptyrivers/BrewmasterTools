4d2381782da98c3dcc73e183ea7b0b2d6e5103d0
emptyrivers <afaure6@gmail.com>
2017-08-10 13:28:34 -0500

    moved to the alpha branch, will merge back in when implemented

--- load.lua
+++
@@ -1,3 +0,0 @@
---Copyright (c) 2017 by Rivers. See ..\LICENSE.md for details 
-
---this part contains all of the code that handles ADDON_LOADED and PLAYER_LOGIN
--- remoteExpel.lua
+++
@@ -1,4 +0,0 @@
---Copyright (c) 2017 by Rivers. See ..\LICENSE.md for details
-
---this holds code for sending information about expel harm;
---basically, this communicates with healers
