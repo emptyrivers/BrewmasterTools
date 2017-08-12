b3c7fddc3e620c4daaa800399bd35815889d1012
emptyrivers <afaure6@gmail.com>
2017-08-12 11:55:37 -0500

    basic load function
    Note that this commit **will** break alpha. Do not try to load
    BrewmasterTools at this point in history.

--- load.lua
+++ load.lua
@@ -1,3 +1,21 @@
 --Copyright (c) 2017 by Rivers. See ..\LICENSE.md for details
 
 --this part contains all of the code that handles ADDON_LOADED and PLAYER_LOGIN
+
+local loadFrame = CreateFrame("FRAME")
+
+loadFrame:RegisterEvent("ADDON_LOADED")
+loadFrame:SetScript("OnEvent",
+  function(event,arg)
+    if event == "ADDON_LOADED" and arg = "BrewmasterTools" then
+      --call init functions for each module, replace dummy functions
+      for _, module in pairs(BrewmasterTools.modules) do
+        module:init()
+        for name, func in pairs(module.api) do
+          BrewmasterTools[name] = func
+        end
+      end
+    end
+
+  end
+)
